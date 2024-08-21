5dd6491c648f3fc3121734bb4a9d7020c7082c34
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-08-21 10:56:11 +0530

    UPSC news for CSE aspirent

--- src/Components/News.js
+++ src/Components/News.js
@@ -22,7 +22,7 @@ export class News extends Component {
                         {/* news */}
                         <NewsIteam title= "Supreme Court's Role in Upholding Civil Liberties" 
                         discription= "the Supreme Court of India's recent decision to grant bail in Delhi excise policy case, emphasizing that liberty is an intrinsic part of constitutionalism, highlights its role in upholding individual rights and ensuring timely justice. " 
-                        imageURL="https://media.licdn.com/dms/image/v2/D5612AQERxGuv7ogxTw/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1707434226590?e=2147483647&v=beta&t=w-YnSFI-M3Ce3UUQOBVjfAcXU5JFut-EvGWbLUK5aKY" 
+                        imageURL="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Supreme_Court_of_India_-_Central_Wing.jpg/220px-Supreme_Court_of_India_-_Central_Wing.jpg" 
                         Source= "Dristi IAS"
                         Detail= "https://www.drishtiias.com/daily-updates/daily-news-editorials/supreme-court-s-role-in-upholding-civil-liberties"/>
                     </div>
