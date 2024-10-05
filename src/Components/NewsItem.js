84d444a1f56f70c66ec9239d11afc6fb809e8e60
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-10-06 00:25:21 +0530

    October 06

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsIteam extends Component {
     render() {
-        let {title, discription, imageURL, Source,Source_Two, Detail,  Detail_Two}= this.props;
+        let {title, discription, imageURL, Source,Detail }= this.props;
         return(
         
                 <div div className="card" style= {{width: "18rem"}}>
                     <img src={imageURL} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{discription}... </p>
+                        <h5 className="card-title">{title.slice(0, 50)}</h5>
+                        <p className="card-text">{discription.slice(0, 99)}... </p>
                         <p>Source-{Source}</p>
-                        <div><a href={Detail} className="btn btn-primary">Detail...</a>  </div>
-                        <p>Source_Two-{Source_Two}</p>
-                        <a href={Detail_Two} className="btn btn-primary">Detail_Two...</a>
+                        <div><a href={Detail} target="blank" className="btn btn-primary">Detail...</a>  </div>
+                       
                     </div>
                     </div>
                     
--- src/Components/NewsItem2.js
+++ src/Components/NewsItem2.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react'
 
 export default class NewsItem2 extends Component {
   render() {
-    return (
-      <div>
+    let {title, discription, imageURL, Source,Source_Two, Detail,  Detail_Two}= this.props;
+    return(
+    
             <div div className="card" style= {{width: "18rem"}}>
-                    <img src={imageURL} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{discription}... </p>
-                        <p>Source-{Source}</p>
-                        <a href={Detail} className="btn btn-primary">Detail...</a>
-                        <p>Source_Two-{Source_Two}</p>
-                        <a href={Detail_Two} className="btn btn-primary">Detail_Two...</a>
-                    </div>
-            </div>
-        
-      </div>
+                <img src={imageURL} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{discription}... </p>
+                    <p>Source-{Source}</p>
+                    <div><a href={Detail} className="btn btn-primary">Detail...</a>  </div>
+                    <p>Source_Two-{Source_Two}</p>
+                    <a href={Detail_Two} className="btn btn-primary">Detail_Two...</a>
+                </div>
+                </div>
+                
     )
   }
 }
